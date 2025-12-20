64f560d0e96d93a309cb53aac7034eaf1e9cb328
Nihal shende <nihalshende465@gmail.com>
2025-12-20 16:30:19 +0530

    Add map

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -192,15 +192,15 @@ const Contact = () => {
       {/* Google Maps Section */}
       <section className="py-0">
         <div className="w-full h-[400px] md:h-[500px]">
-          <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3721.246471840001!2d79.0881512!3d21.1425888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4c09424268953%3A0xc6c42171171804f3!2sShastri%20Nagar%20Square%2C%20Nagpur%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin" 
-            width="100%" 
-            height="100%" 
-            style={{ border: 0 }} 
-            allowFullScreen={true} 
-            loading="lazy" 
-            title="Office Location" 
-          />
+         <iframe 
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3721.201484252157!2d79.0911!3d21.1458!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjHCsDA4JzQ0LjkiTiA3OcKwMDUnMjguMCJF!5e0!3m2!1sen!2sin!4v1703000000000!5m2!1sen!2sin" 
+  width="100%" 
+  height="100%" 
+  style={{ border: 0 }} 
+  allowFullScreen={true} 
+  loading="lazy" 
+  title="Office Location" 
+/>
         </div>
       </section>
     </>
