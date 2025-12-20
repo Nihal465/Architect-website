0639f62b1e09af6bd6d9e6e6d17bdc48c8991c4a
Nihal shende <nihalshende465@gmail.com>
2025-12-20 16:35:01 +0530

    add map correctly

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -192,14 +192,15 @@ const Contact = () => {
       {/* Google Maps Section */}
       <section className="py-0">
         <div className="w-full h-[400px] md:h-[500px]">
-         <iframe 
-  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3721.201484252157!2d79.0911!3d21.1458!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjHCsDA4JzQ0LjkiTiA3OcKwMDUnMjguMCJF!5e0!3m2!1sen!2sin!4v1703000000000!5m2!1sen!2sin" 
+    <iframe 
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3721.284758994511!2d79.1303867750346!3d21.141062080536787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4c7333d455447%3A0xc47b973347c4091a!2sNishant%20Pethe%20And%20Associates!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin" 
   width="100%" 
   height="100%" 
   style={{ border: 0 }} 
   allowFullScreen={true} 
   loading="lazy" 
-  title="Office Location" 
+  referrerPolicy="no-referrer-when-downgrade"
+  title="Office Location"
 />
         </div>
       </section>
